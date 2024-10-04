a2fd3f76d164e0e8cc777652f2ecccaa51072c32
0hz0-code <lfyz170609@gmail.com>
2024-10-04 20:22:14 +0600

    edit: part-1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         var header = document.querySelector('.header');
-        if (window.scrollY > 50) {  // Если прокрутка больше 50px
+        if (window.scrollY > 50) {  // If scroll is greater than 50px
             header.classList.add('scrolled');
+            // Change logo to a different one
+            document.querySelector('.logo img').src = './img/logo.png';
         } else {
             header.classList.remove('scrolled');
+            // Change logo back to original one
+            document.querySelector('.logo img').src = './img/log0.png';
         }
     });
 });
